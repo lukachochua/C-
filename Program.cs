504028e361d2f4b0ec7712cec48e31e3bd45cf36
lukachochua <lukachochua@gmail.com>
2024-04-22 12:55:52 +0400

    feat: Add decision logic to your code using 'if', 'else' and 'else if' statements in C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired!");
+}
+else if (daysUntilExpiration < 2)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription expires within a day!");
+    Console.WriteLine($"Renew now and save {discountPercentage}!");
+}
+else if (daysUntilExpiration < 6)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription will expire in {daysUntilExpiration} days.");
+    Console.WriteLine($"Renew now and save {discountPercentage}!");
+}
+else if (daysUntilExpiration < 11)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
+
+
