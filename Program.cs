e99eb7710105ed2602d43eb415e30362f3c72229
lukachochua <lukachochua@gmail.com>
2024-04-25 14:59:37 +0400

    feat: take an array of strings that contain sentences with periods and split these strings into individual sentences

--- Program.cs
+++ Program.cs
@@ -1,4 +1,41 @@
-﻿bool correctInput = false;
+﻿/* string? readResult;
+
+string valueEntered = "";
+int numericValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Provide an integer value between 5 and 10.");
+
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        valueEntered = readResult;
+    }
+
+    validNumber = int.TryParse(valueEntered, out numericValue);
+
+    if (validNumber == true)
+    {
+        if (numericValue < 5 || numericValue > 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entedered {numericValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again.");
+    }
+
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+
+readResult = Console.ReadLine();
+
+bool correctInput = false;
 string? readLine;
 string role = "";
 
@@ -23,7 +60,40 @@ while (correctInput == false)
     }
 }
 
-Console.WriteLine($"Your input value ({role}) has been accepted.");
+Console.WriteLine($"Your input value ({role}) has been accepted."); */
 
-readLine = Console.ReadLine();
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < myStrings.Length; i++)
+{
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    while (periodLocation != -1)
+    {
+        mySentence = myString.Remove(periodLocation);
+
+        myString = myString.Substring(periodLocation + 1);
+
+        myString = myString.TrimStart();
+
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
+
+
+
+
 
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc106c9b130fed630ef5b6c11f9395ea075e841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03d351c74d81416c8c754c6be0f191ec83fc9e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
