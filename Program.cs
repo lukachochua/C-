0a568449aff9983ac6dca7f46f5323ae5f0a63ad
lukachochua <lukachochua@gmail.com>
2024-04-25 12:20:37 +0400

    feat: exercise - role playing game battle challenge

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿Random rnd = new Random();
+
+int attack = rnd.Next(1, 11);
+
+int Hero = 10;
+int Monster = 10;
+
+
+
+do
 {
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        Console.WriteLine(i + " " + "FizzBuzz");
-    }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine(i + " " + "Fizz");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine(i + " " + "Buzz");
-    }
-    else
-    {
-        Console.WriteLine(i);
-    }
-}
+    // Hero Attacks
+    attack = rnd.Next(1, 11);
+    Monster -= attack;
+    Console.WriteLine($"Monster was damaged and lost {attack} health and now has {Monster} health.");
+
+    if (Monster <= 0) break;
+
+    // Monster Attacks
+    attack = rnd.Next(1, 11);
+    Hero -= attack;
+    Console.WriteLine($"Hero was damaged and lost {attack} health and now has {Hero} health.");
+    if (Hero <= 0) break;
+} while (Hero >= 0 || Monster >= 0);
+
+if (Monster <= 0)
+    Console.WriteLine("Hero Wins");
+else if (Hero <= 0)
+    Console.WriteLine("Monster Wins");
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9dd0e68e1406f0a6fbfaa0a725cedffd35cb7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47e8902e0d045a3c364afc74f023b0ca197ae12")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
