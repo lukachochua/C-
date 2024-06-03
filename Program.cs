28a26b51964fab62743fcebafe9909f1bebb2434
lukachochua <lukachochua@gmail.com>
2024-06-03 13:02:18 +0400

    feat: Get started with C#, Part 4 -  Perform operations on arrays using helper methods in C#. first exercise

--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
-﻿int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+﻿// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
 
-// Your code here to set result1
+// // string result = new(valueArray);
+// string result = String.Join(", ", valueArray);
+// Console.WriteLine(result);
 
-int result1 = Convert.ToInt32(value1 / value2);
-// Hint: You need to round the result to nearest integer (don't just truncate)
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+// string[] items = result.Split(", ");
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
 
-// Your code here to set result2
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] wordPangrams = pangram.Split(" ");
 
-// Your code here to set result3
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+string resultString = "";
+
+for (int i = 0; i < wordPangrams.Length; i++)
+{
+    char[] letters = wordPangrams[i].ToCharArray();
+    string? result = "";
+    Array.Reverse(letters);
+
+    result = new(letters);
+    resultString += result + " ";
+}
+
+Console.WriteLine(resultString);
