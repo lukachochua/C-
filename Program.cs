e857593185f859fa8e4044d15d0c014184943e4f
lukachochua <lukachochua@gmail.com>
2024-06-04 14:42:18 +0400

    feat: Get started with C#, Part 4,  Modify the content of strings using built-in string data type methods in C# - exercise one

--- Program.cs
+++ Program.cs
@@ -1,23 +1,35 @@
-﻿string customerName = "Ms. Barros";
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+string quantity = "";
+string output = "";
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
 
-// Your logic here
-Console.WriteLine($"Dear {customerName},");
-Console.WriteLine("As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
-Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
-Console.WriteLine($"Our new product, Glorious Future offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
-Console.WriteLine("Here's a quick comparison:\n");
+// Your work here
+// Quantity
+string openSpan = "<span>";
 
-string comparisonMessage = $"{currentProduct.PadRight(18)}{currentReturn:p}\t{currentProfit:C}\n{newProduct.PadRight(18)}{newReturn:P}\t{newProfit:C}";
+int quantityIndex = input.IndexOf("<span>");
+int quantityEnd = input.IndexOf("</span>");
 
-// Your logic here
+int openingPos = quantityIndex + openSpan.Length;
 
-Console.WriteLine($"{comparisonMessage}");
+int endingPos = quantityEnd - openingPos;
+
+quantity = "Quantity: " + input.Substring(openingPos, endingPos);
+
+//trade symbol
+
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+// h2
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8bf8dd3e65db3cfd65636b6c1d44e5196606be7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038b8545f47d253865aee27db7a68bfe74d56a3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
